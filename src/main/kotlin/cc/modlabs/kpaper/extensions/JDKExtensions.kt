97397ae479f491731f0ec4af5ee1e09c863960e5
InvalidJoker <0limitdev@gmail.com>
2025-01-31 14:52:35 +0100

    getThisLogger

--- src/main/kotlin/cc/modlabs/kpaper/extensions/JDKExtensions.kt
+++ src/main/kotlin/cc/modlabs/kpaper/extensions/JDKExtensions.kt
@@ -13,6 +13,10 @@ fun getInternalLogger(): org.slf4j.Logger {
     return LoggerFactory.getLogger("cc.modlabs.kpaper")
 }
 
+fun <T : Any> T.getThisLogger(): org.slf4j.Logger {
+    return LoggerFactory.getLogger(this::class.java)
+}
+
 fun <T : Any> T.nullIf(condition: (T) -> Boolean): T? {
     return if (condition(this)) null else this
 }
