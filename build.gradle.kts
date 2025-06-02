e7a8cccf82324dc27389e5873ad2bc8a780ef832
Liam Sage <mleaked@gmail.com>
2025-06-02 12:16:45 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
 
     api("dev.fruxz:stacked:2024.1.1") // TODO: Own implementation
 
-    api("cc.modlabs:KlassicX:2025.4.22.1239")
+    api("cc.modlabs:KlassicX:2025.6.2.1012")
 
     api("com.squareup.okhttp3:okhttp:4.12.0")
 
