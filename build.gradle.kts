50225d38482ab54ab4d970416a69c8ca9e4fe0b7
Liam Sage <mleaked@gmail.com>
2025-11-27 22:55:30 +0100

    Update KlassicX dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
 
     api("dev.fruxz:stacked:2024.1.1")
 
-    api("cc.modlabs:KlassicX:2025.11.27.1632")
+    api("cc.modlabs:KlassicX:2025.11.27.2152")
 
     api("com.squareup.okhttp3:okhttp:4.12.0")
 
